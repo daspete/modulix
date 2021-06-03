7e198794457619bcc2f99f28285e59e609b257ca
daspete <daspetemail@gmail.com>
2021-06-03 16:37:31 +0200

    fix: fixed jiti modules

--- packages/modulix-cli/bin/index.js
+++ packages/modulix-cli/bin/index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
+const jiti = require('jiti')(__filename)
 
-require('../src/index.js').run(process.argv.slice(2))
+jiti('../src/index.js').run(process.argv.slice(2))
     .catch((err) => {
         console.error(err)
     })
